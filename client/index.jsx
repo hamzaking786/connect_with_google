c79580866f3d60e057ce760c9c8e051301e2142b
Hamza Dastgir <m.hamzadastgir@outlook.com>
2022-03-14 17:05:02 +0100

    Get access token and store in a cookie

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
@@ -49,13 +49,32 @@ function Login() {
   );
 }
 
+function LoginCallback() {
+  useEffect(async () => {
+    const { access_token } = Object.fromEntries(
+      new URLSearchParams(window.location.hash.substring(1))
+    );
+    console.log(access_token);
+
+    await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ access_token }),
+    });
+  });
+
+  return <h1>Login callback</h1>;
+}
+
 function Application() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<FrontPage />} />
         <Route path={"/login"} element={<Login />} />
-        <Route path={"/login/callback"} element={<h1>Login callback</h1>} />
+        <Route path={"/login/callback"} element={<LoginCallback />} />
         <Route path={"/profile"} element={<h1>Profile</h1>} />
       </Routes>
     </BrowserRouter>
